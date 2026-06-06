a13a93bcfddc27607895d16e9dba2a036412d998
Arpan Nath <arpan.nath1001@gmail.com>
2026-06-06 17:28:28 +0530

    added comments for the MODULE 1 and structured some code practices

--- app/llm/evaluator.py
+++ app/llm/evaluator.py
@@ -5,17 +5,14 @@ def evaluate_answer(question, answer):
     prompt = open("app\prompts\evaluator_prompt.txt", encoding="utf-8").read()
     response = chat(
         model="gemma4:e2b",
+
         messages=[
             {"role": "system", "content": prompt},
             {
                 "role": "user",
-                "content": f"""
-Question:
-{question}
-
-Candidate Answer:
-{answer}
-""",
+                "content": f"""Question:{question}
+                               Candidate Answer:{answer}
+                            """,
             },
         ],
     )
--- app/llm/interviewer.py
+++ app/llm/interviewer.py
@@ -2,19 +2,17 @@ from ollama import chat
 
 
 def generate_question():
-
-    response = chat(
+    prompt = open("app\prompts\interviewer_prompt.txt",
+                  encoding="utf-8").read()
+    response = chat(  # this si the actual LLM call;
         model="gemma4:e2b",
+
         messages=[
             {
-                "role": "system",
-                "content": open(
-                    "app\prompts\interviewer_prompt.txt",
-                    encoding="utf-8"
-                ).read()
-            },
+                "role": "system",  # this is the "SYSTEM MESSAGE", with this the LLM will behave like a INTERVIEWER as per the "interviewer_prompt.txt";
+                "content": prompt},
             {
-                "role": "user",
+                "role": "user",  # this is the "USER MESSAGE", ask the question;
                 "content": "Ask me one interview question."
             }
         ]
